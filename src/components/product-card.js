b8ed0a33a1593adcdcb6af4ac04d205691dd2399
MuneebNazir <mmnazir157@gmail.com>
2022-12-30 15:36:10 +0500

    added detail page

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class ProductCard extends Component {
   render() {
     const { id, name, img, price, price_unit } = this.props;
     return (
       <>
-        <img src={img}></img>
-        <span>{name}</span>
-        <span>{price}</span>
-        <span>{price_unit}</span>
+        <div onClick={handleClick}>
+          <Link to="/detail">
+            <img src={img}></img>
+            <span>{name}</span>
+            <span>{price}</span>
+            <span>{price_unit}</span>
+          </Link>
+        </div>
       </>
     );
   }
